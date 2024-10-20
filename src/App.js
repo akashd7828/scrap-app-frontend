ef6ab05a6ab0c1209ecac6e70ad96b7feef560de
akashd7828 <akashdwivedi7828@gmail.com>
2024-10-20 15:38:35 +0530

    5kg

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Sharktank from './Components/SharkTan/Sharktank';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Sharktank from "./Components/SharkTan/Sharktank";
 // import Card from './Components/Card/Card';
-import CardComponents from './Components/Card/Card';
-import Leading_Brands from './Components/Leading_Brands/Leading_Brands';
-import TestimonialCard from './Components/Testimonials/Testimonial';
-import Blogs from './Components/Blogs/Blogs';
-import Testing from './Components/Testing/Testing';
-import Form from './Components/Form/Form';
+import CardComponents from "./Components/Card/Card";
+import Leading_Brands from "./Components/Leading_Brands/Leading_Brands";
+import TestimonialCard from "./Components/Testimonials/Testimonial";
+import Blogs from "./Components/Blogs/Blogs";
+import Testing from "./Components/Testing/Testing";
+import Form from "./Components/Form/Form";
 // import Footer from './Components/Footer/FooterRender2';
-import { FooterContainer } from './Components/Footer/Footer';
-import Form1 from './Components/Login/Signup';
-import Signup from './Components/Login/Signup';
-import Login from './Components/Login/Login';
-import MainRoutes from './Components/Routes/MainRoutes';
-import Multistep from './Components/Login/Scrap';
+import { FooterContainer } from "./Components/Footer/Footer";
+import Form1 from "./Components/Login/Signup";
+import Signup from "./Components/Login/Signup";
+import Login from "./Components/Login/Login";
+import MainRoutes from "./Components/Routes/MainRoutes";
+import Multistep from "./Components/Login/Scrap";
 // import GridBlurredBackdrop from './Components/ScrapCards/ScrapCards';
-import LargeWithLogoCentered from './Components/Footer2/Footer';
-import SocialProfileWithImage from './Components/ScrapCards/ScrapCards';
-import MainAdmin from './Admin/Admin';
-import Homepage from './Homepage/Homepage';
-
+import LargeWithLogoCentered from "./Components/Footer2/Footer";
+import SocialProfileWithImage from "./Components/ScrapCards/ScrapCards";
+import MainAdmin from "./Admin/Admin";
+import Homepage from "./Homepage/Homepage";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const element = document.querySelector(".tooltip_wrapper__vA_JM");
+    console.log("@@ele", element);
+    if (element) {
+      element.style.display = "none";
+    }
+  }, []);
   return (
     <div className="App">
-      <Navbar/>
-      <MainRoutes/>
+      <Navbar />
+      <MainRoutes />
 
       {/* <Navbar/>
       <Form/>
